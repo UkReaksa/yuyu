d828a3628fd827d13584d434f6dadc2a5b2b2bcc
UkReaksa <khsa598@gmail.com>
2024-02-26 21:09:33 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,8 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+      <input type="text">
+      <button>click</button>
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
